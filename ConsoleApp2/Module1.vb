b98a0bb8ddebfbaefdd4b28874d88b45d41a15fb
BRAYAN ALEXANDER RAGON LINARES <brayan05012@gmail.com>
2024-09-19 18:35:00 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numero As Double
+        Console.Write("Ingrese un numero: ")
+        numero = Double.Parse(Console.ReadLine())
+
+        If (numero >= 0) Then
+
+            Console.WriteLine(" Es un numero positivo")
+
+        Else Console.WriteLine("Es un numero negativo")
+
+        End If
+    End Sub
+
+End Module
